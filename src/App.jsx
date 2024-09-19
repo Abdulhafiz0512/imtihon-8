beb47b9d060080947b5d9056d591dbc5ea5a239c
Abdulkhafiz Yoqubjonov <psfer10green10@gmail.com>
2024-09-19 21:23:55 +0500

    fixed

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ function App() {
   const [isOpen, setIsOpen] = useState(false);
   const [currency, setCurrency] = useState("usd"); 
   return (
-    <div className="bg-[#14161A] ">
+    <div className="bg-[#14161A] min-h-screen">
       <Router>
         <Header setIsOpen={setIsOpen} setCurrency={setCurrency} currency={currency}/>
         <Routes>
